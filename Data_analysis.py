c8958d60da520277b302c4eacc940782ac14a005
karthik <mrkk2895@gmail.com>
2025-07-16 21:58:06 +0530

    Update Data_analysis.py

--- Data_analysis.py
+++ Data_analysis.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns 
+def smoke_vs_age(f):
+  sns.boxplot(x='Smoking', y='Age', data=f)  #used to visualize the relation between age and smoking
+  plt.title('Smoking Distribution by Age')
+  plt.show()
 
 df = pd.read_csv('thyroid_cancer_risk_data.csv')
-
+smoke_vs_age(df)
